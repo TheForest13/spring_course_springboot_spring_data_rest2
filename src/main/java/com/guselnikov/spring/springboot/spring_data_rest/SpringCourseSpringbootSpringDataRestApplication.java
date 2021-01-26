67a0f86eb9d4588bd2056acc670daaf9aea77e2a
theforest <guselnikow1999@gmail.com>
2021-01-26 17:10:28 +0300

    Initial commit

---
+++ src/main/java/com/guselnikov/spring/springboot/spring_data_rest/SpringCourseSpringbootSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.guselnikov.spring.springboot.spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSpringbootSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseSpringbootSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guselnikov/spring/springboot/spring_data_rest/SpringCourseSpringbootSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guselnikov.spring.springboot.spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseSpringbootSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
